47807dc730b0cbb4b18995d56abc067b6280649e
davsip <david.sipala@gmail.com>
2018-05-15 17:25:33 -0500

    created database, and some other changes

---
+++ db/schema.sql
@@ -0,0 +1,85 @@
+CREATE DATABASE commodities_db 
+USE commodities_db 
+
+
+CREATE DATABASE commodities_db ;
+CREATE TABLE gold 
+(
+ 
+     id int not null AUTO_INCRMENT, 
+     Price decimal (5,2) not null, 
+     Daily chan percentage(5,2) not null, 
+     Opened decimal(5,2) not null, 
+     Daily High decimal (5,2) not null,
+     Daily Low decimal (5,2),not null, 
+     PRIMARY KEY (ID) 
+         
+
+) 
+
+CREATE TABLE silver
+(
+     
+     id int not null AUTO_INCRMENT, 
+     Price decimal (5,2) not null, 
+     Daily change decimal(5,2) not null, 
+     Opened decimal(5,2) not null, 
+     Daily High decimal (5,2) not null,
+     Daily Low decimal (5,2),not null, 
+     PRIMARY KEY (ID)    
+
+) 
+
+
+CREATE TABLE crude oil 
+(
+    
+     id int not null AUTO_INCRMENT, 
+     Price decimal (5,2) not null, 
+     Daily change decimal(5,2) not null, 
+     Opened decimal(5,2) not null, 
+     Daily High decimal (5,2) not null,
+     Daily Low decimal (5,2),not null, 
+     PRIMARY KEY (ID) 
+     
+) 
+
+CREATE TABLE  natural gas 
+(
+     
+     id int not null AUTO_INCRMENT, 
+     Price decimal (5,2) not null, 
+     Daily change decimal(5,2) not null, 
+     Opened decimal(5,2) not null, 
+     Daily High decimal (5,2) not null,
+     Daily Low decimal (5,2),not null, 
+     PRIMARY KEY (ID) 
+     
+) 
+
+
+CREATE TABLE corn 
+(
+     
+     id int not null AUTO_INCRMENT, 
+     Price decimal (5,2) not null, 
+     Daily change decimal(5,2) not null, 
+     Opened decimal(5,2) not null, 
+     Daily High decimal (5,2) not null,
+     Daily Low decimal (5,2),not null, 
+     PRIMARY KEY (ID) 
+     
+) 
+
+CREATE TABLE soybean
+(
+     
+     id int not null AUTO_INCRMENT,
+     Price decimal (5,2) not null, 
+     Daily change decimal(5,2) not null, 
+     Opened decimal(5,2) not null, 
+     Daily High decimal (5,2) not null,
+     Daily Low decimal (5,2),not null, 
+     PRIMARY KEY (ID) 
+     
+) 
